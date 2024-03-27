a73d7e25300257867711254cc7355d4e4718481b
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-03-26 23:31:34 -0300

    added page controllers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Expense Tracker</title>
+
+    <!-- Link to external CSS file -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body>
 
@@ -17,6 +20,5 @@
 
     @include('layouts.footer')
 
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,6 @@
 <footer>
-    <p>&copy; 2020 Expense Tracker</p>
+    <p>
+        &copy; 2024 Expense Tracker made by
+        <a href="https://nicolas-castellano.com/portfolio/">Nicolas Castellano</a>
+    </p>
 </footer>
